4959990077c7671cdf3eb603a51ba2be437bf57c
tykees <balogun.olu.sam@gmail.com>
2024-08-11 23:29:29 +0100

    apos

--- src/app/(router)/dashboard/_components/WelcomeBanner.jsx
+++ src/app/(router)/dashboard/_components/WelcomeBanner.jsx
@@ -8,7 +8,7 @@ import Image from 'next/image';
         <div>
             <h2 className='text-[32px] font-light text-white'> Welcome Back 
             <span className='font-bold text-blue-300'> {user?.user?.fullName}</span></h2>
-            <h2 className='text-[16px] font-light text-slate-300'>Don't stop learning!</h2>
+            <h2 className='text-[16px] font-light text-slate-300'>Don&apos;t stop learning!</h2>
         </div>
      </div>
    )
