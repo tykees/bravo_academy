a40067b9b5d8760ef3905fa1b02b754417ca4f6c
tykees <balogun.olu.sam@gmail.com>
2024-08-12 08:45:28 +0100

    pos

--- src/components/Ready.jsx
+++ src/components/Ready.jsx
@@ -7,7 +7,7 @@ const Ready = () => {
         <div className='flex justify-around h-[200px] items-center bg-sky-600'>
             <div className='text-white ml-2'>
                 <h1 className='text-3xl font-bold'>Ready to Join</h1>
-                <p className='mt-4 w-[320px]'>Don't hesitate, start now. Your success is close.</p>
+                <p className='mt-4 w-[320px]'>Don&apos;t hesitate, start now. Your success is close.</p>
             </div>
             <div className='text-center mt-10'>
                <Link href={'./courses'}> <button className='text-white bg-sky-500 w-[150px]  md:w-[200px] rounded-md h-[50px]'>See more courses</button></Link>
